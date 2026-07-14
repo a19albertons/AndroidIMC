f3d7ee214969b817575864de1ca57203a90fea28
a19albertons <a19albertons@iessanclemente.net>
2026-07-14 17:58:11 +0200

    Documentar clases de la aplicación

--- AndroidIMCApp/app/src/main/java/com/example/androidimc/ImcCalculatorFragment.kt
+++ AndroidIMCApp/app/src/main/java/com/example/androidimc/ImcCalculatorFragment.kt
@@ -8,19 +8,32 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.androidimc.databinding.FragmentImcCalculatorBinding
 
+/**
+ * Fragmento que actua como formulario para calcular tu IMC
+ */
 class ImcCalculatorFragment : Fragment() {
+    // Instancia del binding
     private var _binding: FragmentImcCalculatorBinding? = null
     val binding: FragmentImcCalculatorBinding
         get() = _binding!!
 
+    /**
+     * Crea la vista del fragmento
+     *
+     * @param inflater El inflador de la vista
+     * @param container El contenedor de la vista
+     * @param savedInstanceState El estado de la instancia
+     */
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentImcCalculatorBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        // Reduce la edad en 1 al clickar en el boton
         binding.menosEdad.setOnClickListener {
             binding.edadValor.text =
                 (
@@ -29,6 +42,8 @@ class ImcCalculatorFragment : Fragment() {
                         .toInt() - 1
                 ).toString()
         }
+
+        // Aumenta la edad en 1 al clickar en el boton
         binding.masEdad.setOnClickListener {
             binding.edadValor.text =
                 (
@@ -37,6 +52,8 @@ class ImcCalculatorFragment : Fragment() {
                         .toInt() + 1
                 ).toString()
         }
+
+        // Reduce el peso en 1 al clickar en el boton
         binding.menosPeso.setOnClickListener {
             binding.pesoValor.text =
                 (
@@ -45,6 +62,8 @@ class ImcCalculatorFragment : Fragment() {
                         .toInt() - 1
                 ).toString()
         }
+
+        // Aumenta el peso en 1 al clickar en el boton
         binding.masPeso.setOnClickListener {
             binding.pesoValor.text =
                 (
@@ -53,9 +72,13 @@ class ImcCalculatorFragment : Fragment() {
                         .toInt() + 1
                 ).toString()
         }
+
+        // Cambia el valor de la altura al cambiar el valor del slider
         binding.sliderAltura.addOnChangeListener { _, value, _ ->
             binding.altura.text = "${value.toInt()} cm"
         }
+
+        // Navega a la pantalla de resultados
         binding.moverseResultado.setOnClickListener {
             view.findNavController().navigate(
                 ImcCalculatorFragmentDirections.actionImcCalculatorFragmentToResultIMCFragment(
@@ -72,10 +95,15 @@ class ImcCalculatorFragment : Fragment() {
                 ),
             )
         }
+
+        // Seleccion genero
+        // Hombre
         binding.hombreSelecionado.setOnClickListener {
             binding.hombreSelecionado.setBackgroundTintList(resources.getColorStateList(R.color.selecionado))
             binding.mujerSelecionada.setBackgroundTintList(resources.getColorStateList(R.color.fondo_recuadros))
         }
+
+        // Mujer
         binding.mujerSelecionada.setOnClickListener {
             binding.hombreSelecionado.setBackgroundTintList(resources.getColorStateList(R.color.fondo_recuadros))
             binding.mujerSelecionada.setBackgroundTintList(resources.getColorStateList(R.color.selecionado))
@@ -84,6 +112,9 @@ class ImcCalculatorFragment : Fragment() {
         return view
     }
 
+    /**
+     * Destruye la vista del fragmento
+     */
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
