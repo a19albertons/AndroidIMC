4f856c337f8cee818e5f718acc6516700b82b7cd
a19albertons <a19albertons@iessanclemente.net>
2026-07-15 16:29:35 +0200

    Eliminación dependencia obsoleta

--- AndroidIMCApp/build.gradle.kts
+++ AndroidIMCApp/build.gradle.kts
@@ -2,7 +2,6 @@
 plugins {
     // Librerias por defecto de Android
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
 
     // safeargs para pasar argumentos entre fragmentos
     alias(libs.plugins.androidx.navigation.safeargs) apply false
